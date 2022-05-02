1e63f5d802d661855456a8f9f55dabecaff0e2a9
pavel bobylyov <p.bobylyov@movavi.com>
2022-05-03 01:11:21 +0500

    my-spot init commit

---
+++ my-spot/src/routes/index.svelte
@@ -0,0 +1,26 @@
+<script context="module" lang="ts">
+	export const prerender = true;
+</script>
+
+<script lang="ts">
+	
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<section>
+	INDEX
+</section>
+
+<style>
+	section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex: 1;
+	}
+</style>
